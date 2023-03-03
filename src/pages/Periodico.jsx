415524ee695a1e711d3f39d617d4566f34ed7705
pablomoga9 <moyagarcia99@gmail.com>
2023-03-04 00:44:01 +0100

    news titles and anchors

--- src/pages/Periodico.jsx
+++ src/pages/Periodico.jsx
@@ -4,16 +4,33 @@ import styled from 'styled-components'
 
 function Periodico() {
   return (
-      <Container>
-          <div className='imageContainer'>
+    <Container>
+      <div className='imageContainer'>
         <div className='imageText'>
           <h1 data-aos="fade-right" data-aos-duration="1500">Periódico</h1>
         </div>
         <div className='backHome'>
-      
+
         </div>
       </div>
-      </Container>
+      <div className='equipoContainer'>
+          <div className='members'>
+            <div className='member'>
+              <img src="https://img.icons8.com/ios-filled/50/null/school.png" alt="" />
+              <a href='https://redined.educacion.gob.es/xmlui/bitstream/handle/11162/81300/00820113012348.pdf?sequence=1&isAllowed=y' target={'_blank'}>Educación emocional</a>
+            </div>
+            <div className='member'>
+              <img src="https://img.icons8.com/glyph-neue/64/null/books.png" alt="" />
+              <a href='https://dialnet.unirioja.es/servlet/articulo?codigo=2040741' target={'_blank'}>Aprendizaje basado en problemas</a>
+            </div>
+            <div className='member'>
+              <img src="https://img.icons8.com/ios-filled/100/null/graduation-cap.png" alt="" />
+              <a href='https://revistas.ucm.es/index.php/RCED/article/view/61992/4564456553086' target={'_blank'}>La educación personalizada según García Hoz</a>
+            </div>
+
+          </div>
+        </div>
+    </Container>
   )
 }
 
@@ -73,6 +90,40 @@ box-shadow: 10px 3px 54px -6px rgba(201,190,74,0.76);
     background-attachment: fixed;
     margin: -5px;
     filter: blur(4px);
+  }
+
+  .equipoContainer{
+    width: 90%;
+    margin: auto;
+    margin-top: 5%;
+    background: #60e2ff5c;
+    border: 3px solid #00a2ff;
+    padding: 30px;
+    border-radius: 20px;
+    -webkit-box-shadow: 4px 24px 31px -1px rgba(104,81,135,1);
+    -moz-box-shadow: 4px 24px 31px -1px rgba(104,81,135,1);
+    box-shadow: 4px 24px 31px -1px rgba(104,81,135,1);
+    .members{
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 50px;
+    gap:30px;
+    .member{
+      width: 33%;
+      background: #8feaffd1;
+      padding: 20px;
+      border-radius: 20px;
+      -webkit-box-shadow: 4px 7px 31px 4px rgba(107,156,179,1);
+-moz-box-shadow: 4px 7px 31px 4px rgba(107,156,179,1);
+box-shadow: 4px 7px 31px 4px rgba(107,156,179,1);
+      img{
+        max-width: 70px;
+        max-height: 70px;
+      }
+     
+     
+    }
+  }
   }
 `
 
